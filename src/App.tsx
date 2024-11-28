56631ae2cf89bcfc5ab6524f8400827ab25635a5
geniusgo <gogenius28@naver.com>
2024-11-28 20:36:57 +0900

    폴더 구조 설계

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/index.tsx';
+import Edit from './pages/Edit/index.tsx';
+import Details from './pages/Details/index.tsx';
+import NotFound from './pages/NotFound/index.tsx';
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/details/:id' element={<Details />}></Route>
+      <Route path='/new' element={<Edit />}></Route>
+      <Route path='/edit/:id' element={<Edit />}></Route>
+      <Route path='*' element={<NotFound />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
